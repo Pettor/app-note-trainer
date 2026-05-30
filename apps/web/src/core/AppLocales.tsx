c678ff5c759aef5f3894fce94946a60f38a257e5
Petter Hancock <pettor@gmail.com>
2026-05-30 15:24:30 +0200

    feat: translate application to swedish

--- apps/web/src/core/AppLocales.tsx
+++ apps/web/src/core/AppLocales.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 import { useAtomValue } from "jotai";
-import type { MessageFormatElement } from "react-intl";
 import { IntlProvider } from "react-intl";
+import svMessages from "../locales/sv.json";
 import { localeModeAtom } from "./locale/LocaleAtoms";
 import { LOCALE_CONFIG } from "./locale/LocaleConfig";
 import type { LocaleMode } from "./locale/LocaleMode";
@@ -10,9 +10,10 @@ interface Props {
   children: ReactElement;
 }
 
-function loadLocaleData(_locale: LocaleMode): Record<string, MessageFormatElement[]> {
-  switch (_locale) {
+function loadLocaleData(localeMode: LocaleMode): Record<string, string> {
+  switch (localeMode) {
     case "sv":
+      return svMessages;
     case "en":
     default:
       return {};
